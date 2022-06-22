19b1398e8976e8d1ad00eaa72e8cefc5d806e193
emxxjnm <emxxjnm@example>
2022-06-22 11:23:41 +0800

    feat: remove copilot
    No way I'd ever consider paying for this hahaha

--- lua/mvim/plugins.lua
+++ lua/mvim/plugins.lua
@@ -320,6 +320,7 @@ return packer.startup(function(use)
 
   use({
     "github/copilot.vim",
+    disable = true,
     setup = function()
       vim.g.copilot_no_tab_map = true
       vim.api.nvim_set_keymap("i", "<C-j>", "copilot#Accept('<CR>')", { silent = true, expr = true })
