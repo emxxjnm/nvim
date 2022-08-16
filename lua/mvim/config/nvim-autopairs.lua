3f4119dda71bb8f1f98400d44887ff13a8c5e900
emxxjnm <emxxjnm@example>
2022-08-16 10:31:44 +0800

    chore(autopairs): update default config

--- lua/mvim/config/nvim-autopairs.lua
+++ lua/mvim/config/nvim-autopairs.lua
@@ -12,6 +12,18 @@ function M.setup()
       "spectre_panel",
       "dap-repl",
     },
+    disable_in_macro = false,
+    disable_in_viualblock = false,
+    ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
+    enable_moveright = true,
+    ---@usage add bracket pairs after quote
+    enable_afterquote = true,
+    ---@usage map the <BS> key
+    map_bs = true,
+    ---@usage map <c-w> to delete a pair if possible
+    map_c_w = false,
+    ---@usage disable when insert after visual block mode
+    disable_in_visualblock = false,
     fast_wrap = {
       map = "<M-e>",
       chars = { "{", "[", "(", '"', "'" },
@@ -24,6 +36,11 @@ function M.setup()
       highlight_grey = "Comment",
     },
   })
+
+  pcall(function()
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end)
 end
 
 return M
