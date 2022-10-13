c63cb3cfa3f97811d9effeaf7d9b432676727f9e
emxxjnm <emxxjnm@example>
2022-10-13 09:21:22 +0800

    fix(lua-dev): make sure we are loading the correct types

--- lua/mvim/lsp/providers/sumneko_lua.lua
+++ lua/mvim/lsp/providers/sumneko_lua.lua
@@ -1,26 +1,63 @@
-local dev_opts = {
+local default_workspace = {
   library = {
-    enabled = true,
-    vimruntime = true,
-    types = true,
-    plugins = false,
+    vim.fn.expand("$VIMRUNTIME"),
+    vim.fn.stdpath("config"),
+    require("lua-dev.config").types(),
   },
-  setup_jsonls = true,
-  override = nil,
-}
-local ok, luadev = pcall(require, "lua-dev")
 
-if ok then
-  luadev.setup(dev_opts)
+  maxPreload = 5000,
+  preloadFileSize = 10000,
+}
+
+local add_packages_to_workspace = function(packages, config)
+  -- config.settings.Lua = config.settings.Lua or { workspace = default_workspace }
+  local runtimedirs = vim.api.nvim__get_runtime({ "lua" }, true, { is_lua = true })
+  local workspace = config.settings.Lua.workspace
+  for _, v in pairs(runtimedirs) do
+    for _, pack in ipairs(packages) do
+      if v:match(pack) and not vim.tbl_contains(workspace.library, v) then
+        table.insert(workspace.library, v)
+      end
+    end
+  end
 end
+
+local lspconfig = require("lspconfig")
+
+local make_on_new_config = function(on_new_config, _)
+  return lspconfig.util.add_hook_before(on_new_config, function(new_config, _)
+    local server_name = new_config.name
+
+    if server_name ~= "sumneko_lua" then
+      return
+    end
+    local plugins = {
+      "plenary.nvim",
+      "telescope.nvim",
+      "nvim-treesitter",
+      "LuaSnip",
+      "catppuccin",
+      "bufferline.nvim",
+    }
+    add_packages_to_workspace(plugins, new_config)
+  end)
+end
+
+lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
+  on_new_config = make_on_new_config(lspconfig.util.default_config.on_new_config),
+})
 
 local opts = {
   settings = {
     Lua = {
-      format = { enable = false },
-      telemetry = {
-        enable = false,
+      telemetry = { enable = false },
+      runtime = {
+        version = "LuaJIT",
+        special = {
+          reload = "require",
+        },
       },
+      workspace = default_workspace,
     },
   },
 }
