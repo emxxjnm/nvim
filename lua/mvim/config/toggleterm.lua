1daad42b5fe99e881a5e6a089a161571c72a6f26
Myles Mo <mylesmo.ash@gmail.com>
2022-11-10 09:32:13 +0800

    chore(toggleterm): simplified configuration

--- lua/mvim/config/toggleterm.lua
+++ lua/mvim/config/toggleterm.lua
@@ -3,17 +3,13 @@ local M = {}
 function M.setup()
   require("toggleterm").setup({
     open_mapping = [[<c-\>]],
-    hide_numbers = true,
-    shade_filetypes = {},
-    shade_terminals = true,
+    shade_terminals = false,
     shading_factor = 1,
     start_in_insert = true,
     insert_mappings = false,
-    terminal_mappings = true,
     persist_size = false,
     direction = "horizontal",
     autochdir = true,
-    close_on_exit = true,
     highlights = {
       NormalFloat = {
         link = "NormalFloat",
@@ -24,7 +20,7 @@ function M.setup()
     },
     size = function(term)
       if term.direction == "horizontal" then
-        return 15
+        return math.floor(vim.o.lines * 0.4)
       elseif term.direction == "vertical" then
         return math.floor(vim.o.columns * 0.4)
       end
