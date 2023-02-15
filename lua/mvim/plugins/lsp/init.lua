c19ee9aebd9ea5b3123e7fdedac791449e8e1652
Myles Mo <mylesmo.ash@gmail.com>
2023-02-15 10:02:34 +0800

    fix(eslint): revert lsp config

--- lua/mvim/plugins/lsp/init.lua
+++ lua/mvim/plugins/lsp/init.lua
@@ -38,11 +38,11 @@ local M = {
         dockerls = {},
         cssls = {},
         eslint = {
-          on_attach = function()
+          on_attach = function(_, buffer)
             require("mvim.utils").augroup("AutoFixOnSave", {
               {
                 event = "BufWritePre",
-                pattern = { "*.ts", "*.tsx", "*.js", "*.jsx", "*.vue" },
+                buffer = buffer,
                 command = "EslintFixAll",
                 desc = "automatically execute `eslint fix` on save",
               },
