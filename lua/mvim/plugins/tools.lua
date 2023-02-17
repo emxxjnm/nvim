147b8d6b789744a2a5101f394cb9e6a8a6e837d3
Myles Mo <mylesmo.ash@gmail.com>
2023-02-17 10:50:55 +0800

    fix(comment): use ts_context_commentstring intergation

--- lua/mvim/plugins/tools.lua
+++ lua/mvim/plugins/tools.lua
@@ -33,21 +33,7 @@ local M = {
     },
     opts = {
       ignore = "^$",
-      pre_hook = function(ctx)
-        local utils = require("Comment.utils")
-        local type = ctx.ctype == utils.ctype.linewise and "__default" or "__multiline"
-        local location = nil
-        if ctx.ctype == utils.ctype.blockwise then
-          location = require("ts_context_commentstring.utils").get_cursor_location()
-        elseif ctx.cmotion == utils.cmotion.v or ctx.cmotion == utils.cmotion.V then
-          location = require("ts_context_commentstring.utils").get_visual_start_location()
-        end
-
-        return require("ts_context_commentstring.internal").calculate_commentstring({
-          key = type,
-          location = location,
-        })
-      end,
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
     },
   },
 
