e195700f5f10e09d5905a541bf26f1f290b96e0c
Myles Mo <mylesmo.ash@gmail.com>
2023-09-21 11:08:30 +0800

    fix(fold): reformat the end text to trim excess whitespace

--- lua/mvim/plugins/utility.lua
+++ lua/mvim/plugins/utility.lua
@@ -41,6 +41,7 @@ local M = {
     opts = {
       open_fold_hl_timeout = 0,
       enable_get_fold_virt_text = true,
+      close_fold_kinds = { "imports", "comment" },
       preview = {
         win_config = {
           winblend = 0,
@@ -49,23 +50,23 @@ local M = {
         },
       },
       fold_virt_text_handler = function(virt_text, lnum, end_lnum, width, truncate, ctx)
-        local result, cur_width, padding = {}, 0, ""
-        local suffix = (" ---[%d]--- "):format(end_lnum - lnum)
-        local suffix_width = vim.api.nvim_strwidth(ctx.text)
+        local result, cur_width = {}, 0
+        local suffix = (" 󰁂 %d "):format(end_lnum - lnum)
+        local suffix_width = vim.fn.strdisplaywidth(suffix)
         local target_width = width - suffix_width
 
         for _, chunk in ipairs(virt_text) do
           local chunk_text = chunk[1]
-          local chunk_width = vim.api.nvim_strwidth(chunk_text)
+          local chunk_width = vim.fn.strdisplaywidth(chunk_text)
           if target_width > cur_width + chunk_width then
             table.insert(result, chunk)
           else
             chunk_text = truncate(chunk_text, target_width - cur_width)
             local hl_group = chunk[2]
             table.insert(result, { chunk_text, hl_group })
-            chunk_width = vim.api.nvim_strwidth(chunk_text)
+            chunk_width = vim.fn.strdisplaywidth(chunk_text)
             if cur_width + chunk_width < target_width then
-              padding = padding .. (" "):rep(target_width - cur_width - chunk_width)
+              suffix = suffix .. (" "):rep(target_width - cur_width - chunk_width)
             end
             break
           end
@@ -73,14 +74,12 @@ local M = {
         end
 
         local end_text = ctx.get_fold_virt_text(end_lnum)
-        -- reformat the end text to trim excess whitespace from
-        -- indentation usually the first item is indentation
-        if end_text[1] and end_text[1][1] then
-          end_text[1][1] = end_text[1][1]:gsub("[%s\t]+", "")
+        -- reformat the end text to trim excess whitespace
+        for _, chunk_text in pairs(end_text) do
+          chunk_text[1] = chunk_text[1]:gsub("[%s\t]+", "")
         end
 
         vim.list_extend(result, { { suffix, "UfoFoldedEllipsis" }, unpack(end_text) })
-        table.insert(result, { padding, "" })
         return result
       end,
       provider_selector = function()
