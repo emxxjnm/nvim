dd832fa2d4c339b2b0076a088d262e98dba6f517
Myles Mo <mylesmo.ash@gmail.com>
2023-10-12 15:32:22 +0800

    fix(neo-tree): during init check global argslist instead of window-local

--- lua/mvim/plugins/exporter.lua
+++ lua/mvim/plugins/exporter.lua
@@ -13,7 +13,7 @@ local M = {
     },
   },
   init = function()
-    if vim.fn.argc() == 1 then
+    if vim.fn.argc(-1) == 1 then
       local stat = vim.loop.fs_stat(vim.fn.argv(0))
       if stat and stat.type == "directory" then
         require("neo-tree")
