3d1fb689e734d18514523ab3742da148c32df01a
Myles Mo <mylesmo.ash@gmail.com>
2023-12-18 17:06:24 +0800

    chore(plugins): use markdonw-preview.nvim instead of peek.nvim

--- lua/mvim/plugins/notes.lua
+++ lua/mvim/plugins/notes.lua
@@ -2,28 +2,23 @@ local M = {
   {
     "dhruvasagar/vim-table-mode",
     cmd = "TableModeToggle",
-    keys = { { "<leader>mt", "<Cmd>TableModeToggle<CR>", desc = "MarkDown table mode toggle" } },
+    ft = { "markdown" },
+    keys = {
+      { "<leader>mt", "<Cmd>TableModeToggle<CR>", desc = "MarkDown table mode toggle" },
+    },
   },
 
   -- markdown preview
   {
-    "toppair/peek.nvim",
-    build = "deno task --quiet build:fast",
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
     keys = {
-      {
-        "<leader>mp",
-        function()
-          local peek = require("peek")
-          if peek.is_open() then
-            peek.close()
-          else
-            peek.open()
-          end
-        end,
-        desc = "Peek (Markdown Preview)",
-      },
+      { "<leader>mp", "<Cmd>MarkdownPreviewToggle<CR>", desc = "Markdown Preview" },
     },
-    opts = { theme = "dark", app = "browser" },
   },
 }
 
