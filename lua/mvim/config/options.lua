5c8bcbf2e9598508dcef31a015871b22d4b29d86
Myles Mo <mylesmo.ash@gmail.com>
2023-12-27 11:04:25 +0800

    fix: update options

--- lua/mvim/config/options.lua
+++ lua/mvim/config/options.lua
@@ -1,60 +1,60 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Enable auto format
 vim.g.autoformat = true
 
-local o, opt = vim.o, vim.opt
+local opt = vim.opt
 local icons = mo.styles.icons
 local settings = mo.settings
 
--- Indentation
-o.wrap = false
-
-o.expandtab = true
-o.shiftwidth = 2
-o.shiftround = true
-
--- line
-o.number = true
-o.relativenumber = true
-
-o.cursorline = true
-o.cursorlineopt = mo.styles.transparent and "number" or "number,line"
-
--- fold
-o.foldlevel = 99
-o.foldcolumn = "auto"
-o.foldmethod = "expr"
-o.foldexpr = "nvim_treesitter#foldexpr()"
-
--- display
-o.signcolumn = "yes"
--- o.smoothscroll = true
-
--- o.modifiable = true
--- o.fileencoding = "utf-8"
+opt.autowrite = true -- new
 
 opt.clipboard = "unnamedplus"
-
-o.termguicolors = true
-
-o.cmdheight = 1
-
-o.ruler = false
-o.showcmd = false
-o.showmode = false
-
-o.laststatus = 0
-
-o.scrolloff = 7
-o.sidescrolloff = 5
-
-o.pumheight = 12
-
-o.completeopt = "menu,menuone,noselect"
-
-o.confirm = true
-
+opt.completeopt = "menu,menuone,noselect"
+opt.confirm = true
+opt.cursorline = true
+opt.cursorlineopt = mo.styles.transparent and "number" or "number,line"
+opt.expandtab = true
+opt.formatoptions = "jcroqlnt" -- tcqj
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep"
+opt.ignorecase = true
+opt.laststatus = 0 -- 3
+-- opt.list = true -- new
+opt.number = true
+opt.relativenumber = true
+opt.pumheight = 12
+opt.scrolloff = 7
+opt.sidescrolloff = 5
+opt.sessionoptions = {
+  "buffers",
+  "curdir",
+  "tabpages",
+  "winsize",
+  "help",
+  "globals",
+  "skiprtp",
+  "folds",
+}
+opt.shiftround = true
+opt.shiftwidth = 2
+opt.shortmess:append({ W = true, I = true, c = true, C = true })
+opt.showmode = false
+opt.signcolumn = "yes"
+opt.smartcase = true
+opt.smartindent = true -- new
+opt.splitbelow = true
+opt.splitright = true
+opt.splitkeep = "screen" -- new
+opt.tabstop = 2 -- new
+opt.termguicolors = true
+opt.timeoutlen = 300 -- 500
+opt.updatetime = 200 -- 500
+opt.virtualedit = "block" -- new
+opt.wildmode = "longest:full,full"
+opt.winminwidth = 5 -- new
+opt.wrap = false
 opt.fillchars = {
   eob = " ",
   fold = " ",
@@ -64,37 +64,21 @@ opt.fillchars = {
   foldclose = icons.documents.collapsed,
 }
 
-o.wildoptions = "pum"
-o.wildignorecase = true
-o.wildmode = "longest:full,full"
+-- fold
+opt.foldlevel = 99
+opt.foldcolumn = "auto"
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
 
--- match and search
-o.incsearch = true
-o.smartcase = true
-o.ignorecase = true
+opt.showcmd = false
 
--- Timings
-o.timeoutlen = 500
-o.updatetime = 500
+opt.wildoptions = "pum"
 
--- Window splitting
-o.splitright = true
-o.splitbelow = true
+opt.undofile = true
+opt.undodir = { settings.undodir }
 
-o.splitkeep = "screen"
+opt.swapfile = true
+opt.directory = { settings.swapdir }
 
--- Backup and Swap
-o.swapfile = true
-o.directory = settings.swapdir
-
-o.undofile = true
-o.undodir = settings.undodir
-
-o.backup = true
-o.backupdir = mo.settings.backupdir
-
--- Message output on vim actions
-opt.shortmess:append({ W = true, I = true, c = true, C = true })
-
-opt.grepformat = "%f:%l:%c:%m"
-opt.grepprg = "rg --vimgrep"
+opt.backup = true
+opt.backupdir = { mo.settings.backupdir }
