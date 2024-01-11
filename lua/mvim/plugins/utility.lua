09d2ce2743862bd759ce31fc796066b821c996a6
Myles Mo <myles@mojianmings-MacBook-Pro.local>
2024-01-11 17:44:03 +0800

    fix!: remove nvim-colorizer.lua
    It is rarely used

--- lua/mvim/plugins/utility.lua
+++ lua/mvim/plugins/utility.lua
@@ -90,24 +90,6 @@ local M = {
   },
 
   {
-    "NvChad/nvim-colorizer.lua",
-    ft = function()
-      local plugin = require("lazy.core.config").spec.plugins["nvim-colorizer.lua"]
-      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-      return opts.filetypes or {}
-    end,
-    opts = {
-      filetypes = { "vue", "css", "scss", "less", "html" },
-      buftypes = { "*", "!prompt", "!popup" },
-      user_default_options = {
-        names = false,
-        mode = "virtualtext",
-        virtualtext = " ",
-      },
-    },
-  },
-
-  {
     "stevearc/dressing.nvim",
     init = function()
       ---@diagnostic disable-next-line: duplicate-set-field
