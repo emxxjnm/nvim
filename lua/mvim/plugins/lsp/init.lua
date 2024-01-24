96d0c806db855973432aef85265c7f080d64eb0f
Myles Mo <mylesmo.ash@gmail.com>
2024-01-24 17:02:31 +0800

    fix(lsp): remove rust_analyzer

--- lua/mvim/plugins/lsp/init.lua
+++ lua/mvim/plugins/lsp/init.lua
@@ -46,36 +46,6 @@ local M = {
       servers = {
         bashls = {},
         dockerls = {},
-        rust_analyzer = {
-          keys = {
-            { "K", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
-            { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
-            { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
-          },
-          settings = {
-            ["rust-analyzer"] = {
-              cargo = {
-                allFeatures = true,
-                loadOutDirsFromCheck = true,
-                runBuildScripts = true,
-              },
-              -- Add clippy lints for Rust.
-              checkOnSave = {
-                allFeatures = true,
-                command = "clippy",
-                extraArgs = { "--no-deps" },
-              },
-              procMacro = {
-                enable = true,
-                ignored = {
-                  ["async-trait"] = { "async_trait" },
-                  ["napi-derive"] = { "napi" },
-                  ["async-recursion"] = { "async_recursion" },
-                },
-              },
-            },
-          },
-        },
         gopls = {
           settings = {
             gopls = {
