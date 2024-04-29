ed67fc3238bbea5b62fcf3e4639f02f9da17dbee
Myles Mo <mylesmo.ash@gmail.com>
2024-04-29 13:45:21 +0800

    chore!: update todo-comments keymaps

--- lua/mvim/plugins/editor.lua
+++ lua/mvim/plugins/editor.lua
@@ -76,22 +76,11 @@ local M = {
     "folke/todo-comments.nvim",
     cmd = { "TodoTrouble", "TodoTelescope" },
     event = { "BufReadPost" },
+    -- stylua: ignore
     keys = {
-      {
-        "[l",
-        function()
-          require("todo-comments").jump_prev()
-        end,
-        desc = "Prev todo comment",
-      },
-      {
-        "]l",
-        function()
-          require("todo-comments").jump_next()
-        end,
-        desc = "Next todo comment",
-      },
-      { "<leader>fl", "<Cmd>TodoTelescope<CR>", desc = "List todo" },
+      { "<leader>fT", "<Cmd>TodoTelescope<CR>", desc = "Find TODOs" },
+      { "[T", function() require("todo-comments").jump_prev() end, desc = "Prev TODO comment" },
+      { "]T", function() require("todo-comments").jump_next() end, desc = "Next TODO comment" },
     },
     opts = function()
       local palette = require("mvim.config").palette
