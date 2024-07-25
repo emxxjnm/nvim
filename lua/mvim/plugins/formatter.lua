98f75677f11faf5dc972bdf31562eb8d9d5b3681
Myles Mo <mylesmo.ash@gmail.com>
2024-07-25 09:24:39 +0800

    chore(formatter): changes for new config

--- lua/mvim/plugins/formatter.lua
+++ lua/mvim/plugins/formatter.lua
@@ -8,27 +8,17 @@ return {
         Mo.U.format.formatter = {
           name = "conform.nvim",
           format = function(buf)
-            local ft = vim.bo[buf].filetype
-            local opts = Mo.U.opts("conform.nvim")
-            local extra_args = opts.extra_lang_opts[ft] or {}
-            require("conform").format(vim.tbl_deep_extend("force", { bufnr = buf }, extra_args))
+            require("conform").format({ bufnr = buf })
           end,
         }
       end)
     end,
     opts = {
-      extra_lang_opts = {
-        go = {
-          lsp_format = "last",
-        },
-        rust = {
-          lsp_format = "last",
-        },
-      },
       formatters_by_ft = {
         sh = { "shfmt" },
         lua = { "stylua" },
-        go = { "goimports" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        go = { "goimports", lsp_format = "last" },
         javascript = { "eslint_d" },
         typescript = { "eslint_d" },
         vue = { "eslint_d", "stylelint" },
@@ -37,6 +27,9 @@ return {
       formatters = {
         shfmt = { prepend_args = { "-i", "2", "-ci" } },
         eslint_d = {
+          env = {
+            ESLINT_USE_FLAT_CONFIG = "true",
+          },
           condition = function(self, ctx)
             return vim.fs.find(
               { "eslint.config.js", ".eslintrc.cjs" },
