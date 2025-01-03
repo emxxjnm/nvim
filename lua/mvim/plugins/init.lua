3bf15996fd9bef5cb8d143179630ac6f51067f0f
Myles Mo <mylesmo.ash@gmail.com>
2025-01-03 09:33:19 +0800

    fix(snacks): disabled indent for markdown

--- lua/mvim/plugins/init.lua
+++ lua/mvim/plugins/init.lua
@@ -24,6 +24,12 @@ return {
         chunk = {
           enabled = true,
         },
+        filter = function(buf)
+          return vim.g.snacks_indent ~= false
+            and vim.b[buf].snacks_indent ~= false
+            and vim.bo[buf].buftype == ""
+            and vim.bo[buf].filetype ~= "markdown"
+        end,
       },
       scope = { enabled = true },
       notifier = {
