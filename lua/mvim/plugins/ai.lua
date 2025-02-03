6398ce8b6f321abf00e579605a10a9270d4e231a
myles <mylesmo.ash@gmail.com>
2025-02-03 09:31:46 +0800

    fix(CopilotChat): set signcolumn="no"

--- lua/mvim/plugins/ai.lua
+++ lua/mvim/plugins/ai.lua
@@ -75,6 +75,7 @@ return {
         pattern = "copilot-*",
         callback = function()
           vim.opt_local.number = false
+          vim.opt_local.signcolumn = "no"
           vim.opt_local.relativenumber = false
         end,
       })
