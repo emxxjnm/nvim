9bb0266e5476d939bbb30e21f2e54962c78965bc
myles <mylesmo.ash@gmail.com>
2025-02-04 15:05:28 +0800

    fix: use blink-copilot instead of blink-cmp-copilot

--- lua/mvim/plugins/completion.lua
+++ lua/mvim/plugins/completion.lua
@@ -3,9 +3,7 @@ local M = {
     "saghen/blink.cmp",
     event = { "InsertEnter", "CmdlineEnter" },
     build = "cargo build --release",
-    dependencies = {
-      { "giuxtaposition/blink-cmp-copilot" },
-    },
+    dependencies = { "fang2hou/blink-copilot" },
     opts = {
       keymap = {
         -- disable default preset
@@ -65,18 +63,9 @@ local M = {
         providers = {
           copilot = {
             name = "copilot",
-            module = "blink-cmp-copilot",
+            module = "blink-copilot",
             score_offset = 100,
             async = true,
-            transform_items = function(_, items)
-              local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-              local kind_idx = #CompletionItemKind + 1
-              CompletionItemKind[kind_idx] = "Copilot"
-              for _, item in ipairs(items) do
-                item.kind = kind_idx
-              end
-              return items
-            end,
           },
         },
       },
@@ -89,6 +78,7 @@ local M = {
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
+    event = "BufReadPost",
     build = ":Copilot auth",
     opts = {
       suggestion = { enabled = false },
