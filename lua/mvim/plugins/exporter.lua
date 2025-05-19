332825fffb6500e513e3293b8c354b7952eee71f
myles <mylesmo.ash@gmail.com>
2025-05-19 15:47:29 +0800

    fix(neo-tree): update git status when exiting lazygit

--- lua/mvim/plugins/exporter.lua
+++ lua/mvim/plugins/exporter.lua
@@ -152,6 +152,17 @@ local M = {
       },
     }
   end,
+  config = function(_, opts)
+    require("neo-tree").setup(opts)
+    vim.api.nvim_create_autocmd("TermClose", {
+      pattern = "*lazygit",
+      callback = function()
+        if package.loaded["neo-tree.sources.git_status"] then
+          require("neo-tree.sources.git_status").refresh()
+        end
+      end,
+    })
+  end,
 }
 
 return M
