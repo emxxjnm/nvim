ef38354e656241e52901569dccc0dc9eb04d8548
myles <mylesmo.ash@gmail.com>
2026-05-20 15:31:01 +0800

    chore: remove mise.toml

--- flake.nix
+++ flake.nix
@@ -1,28 +1,34 @@
 {
   description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.systems.url = "github:nix-systems/default";
-  inputs.flake-utils = {
-    url = "github:numtide/flake-utils";
-    inputs.systems.follows = "systems";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
+  outputs = { fenix, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
+        toolchain = fenix.packages.${system}.minimal.toolchain;
       in {
         devShells.default = pkgs.mkShell {
           name = "nvim";
           packages = with pkgs; [
+            nil
             stylua
+            nixfmt
+            toolchain
+            tree-sitter
             lua-language-server
           ];
         };
-      }
-    );
+      });
 }
