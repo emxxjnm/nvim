01cd2088edadbe525b663b42a412ce7ad23c16b4
myles <mylesmo.ash@gmail.com>
2026-08-20 16:05:56 +0800

    chore: update nvim-surround

--- lua/mvim/plugins/editor.lua
+++ lua/mvim/plugins/editor.lua
@@ -33,28 +33,9 @@ local M = {
 
   {
     "kylechui/nvim-surround",
-    keys = {
-      { "gsa", desc = "Add surround", mode = { "n", "v" } },
-      { "gsc", desc = "Change surround" },
-      { "gsd", desc = "Delete surround" },
-      { "<C-g>s", desc = "Add surround", mode = { "i" } },
-      { "<C-g>S", desc = "Add surround", mode = { "i" } },
-    },
+    event = "VeryLazy",
     opts = {
-      move_cursor = false,
-      keymaps = {
-        insert = "<C-g>s",
-        insert_line = "<C-g>S",
-        normal = "gsa",
-        normal_cur = false,
-        normal_line = false,
-        normal_cur_line = false,
-        visual = "gsa",
-        visual_line = false,
-        delete = "gsd",
-        change = "gsc",
-        change_line = false,
-      },
+      move_cursor = "sticky",
     },
   },
 
